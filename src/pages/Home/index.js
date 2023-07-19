5a181b88fef54582140ff486b81cafba0b478e33
Lucas Aguiar <lucardaguiar@gmail.com>
2023-07-19 17:13:17 -0300

    props no card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,10 +7,10 @@ export function Home() {
         <h1>Lista de Presença</h1>
         <input type="text" placeholder="Digite seu nome"/>
         <button>Adicionar</button>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card name='Dora' time='10:12:15'/>
+        <Card name='Tiago' time='09:06:05'/>
+        <Card name='Osmar' time='23:14:17'/>
+        <Card name='Lucas' time='16:11:16'/>
       </div>
 
     
