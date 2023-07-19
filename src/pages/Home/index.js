4f4762bfb2c568179b949391081357f50c4ed9fa
Lucas Aguiar <lucardaguiar@gmail.com>
2023-07-19 19:37:32 -0300

    adiciona estado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
 import './style.css'
 import { Card } from '../../components/Card'
 
 export function Home() {
+
+    const [name, setName] = useState('Lucas')
+    
     return (
+
       <div className='container'>
-        <h1>Lista de Presença</h1>
-        <input type="text" placeholder="Digite seu nome"/>
+        <h1>{name}</h1>
+        <input 
+        type="text" 
+        placeholder="Digite seu nome"
+        onChange={function(event) {setName(event.target.value)}}
+        />
         <button>Adicionar</button>
         <Card name='Dora' time='10:12:15'/>
         <Card name='Tiago' time='09:06:05'/>
